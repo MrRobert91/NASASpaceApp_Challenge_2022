d3c21a035df6f9cafa5b8c8bdab91e2b77201d82
David Leirado Maroto <dleiradomaroto@gmail.com>
2022-10-01 10:37:31 +0200

    feat!: test api created/ endpoint method GET: /generate

--- api/main.py
+++ api/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
 @app.get("/")
 async def home(request: Request):
     return {"msg":"NASA SpaceApp Challenge!"}
+
+@app.get("/generate")
+async def generate(request: Request):
+    return FileResponse("./api/mars.jpg")
